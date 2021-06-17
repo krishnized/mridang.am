2724becbb49275afb73cd7bed3e0c55518a3dede
dy <df.creative@gmail.com>
2021-06-16 20:22:50 -0400

    Add basic site

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,11 @@
+import 'ress'
+import './main.css'
+import 'plyr/dist/plyr.css'
 import Plyr from 'plyr'
 
-console.log(Plyr)
+
+const playerEl = document.querySelector('#player')
+
+const player = new Plyr(playerEl, {
+  youtube: {controls: 0}
+});
