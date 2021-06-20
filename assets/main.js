647e064380aff5f4db9039b60d8091843bd1d78d
dy <df.creative@gmail.com>
2021-06-20 05:17:10 -0400

    Add dict, fix video

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,25 @@
 import 'mvp.css'
 import './main.css'
 
-import 'vlitejs/dist/vlite.css';
-import Vlitejs from 'vlitejs';
-import VlitejsYoutube from 'vlitejs/dist/providers/youtube';
+// import 'vlitejs/dist/vlite.css';
+// import Vlitejs from 'vlitejs';
+// import VlitejsYoutube from 'vlitejs/dist/providers/youtube';
 
-Vlitejs.registerProvider('youtube', VlitejsYoutube);
+// Vlitejs.registerProvider('youtube', VlitejsYoutube);
 
-const player = new Vlitejs('#player',{
-  options: {
-    playPause: false,
-    time: false,
-    volume: false,
-    fullscreen: false,
-    poster: false,
-    loop: true,
-    providerParams: {start: 1, end: 2}
-  },
-  provider: 'youtube',
-  onReady: async (player) => {
-    // setTimeout(() => player.play(), 1000)
-    // setTimeout(() => player.seekTo(5), 2000)
-  }
-});
+// const player = new Vlitejs('#player',{
+//   options: {
+//     playPause: false,
+//     time: false,
+//     volume: false,
+//     fullscreen: false,
+//     poster: false,
+//     loop: true,
+//     providerParams: {start: 1, end: 2}
+//   },
+//   provider: 'youtube',
+//   onReady: async (player) => {
+//     // setTimeout(() => player.play(), 1000)
+//     // setTimeout(() => player.seekTo(5), 2000)
+//   }
+// });
--- tutorial/mayapur-mridang/tutorial-1.md
+++ tutorial/mayapur-mridang/tutorial-1.md
@@ -2,24 +2,7 @@
 layout: default
 title: Te Re Khe Ta & Variations
 video: "EO4urNqjjRE"
-parts:
-  - title: Mantra
-    clip: []
-    mantra: |
-      TE RE KHE TA
-  - title: Variation 1
-    clip: []
-    mantra: |
-      TE RE KHE TA
-      TE RE TE RE KHE TA
-  - title: Variation 2
-    clip: []
-    mantra: |
-      TE RE KHE TA
-      TE RE KHE TA
-      TE RE TE RE TE RE KHE TA
 ---
-<br/>
 
 {% include mantra-section.html title="Mantra" mantra="Te re khe ta" %}
 <br/>
